bfff9fee337a56a07d154ec4ad27c0881d94caf8
B-HANSEN <b.hansen.ubiqum@gmail.com>
2019-11-04 23:02:17 +0100

    created dummy components

---
+++ src/components/create-exercise.component.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+
+export default class CreateExercises extends Component {
+    // constructor(props){
+    //     super(props)
+
+    //     this.state = {
+    //         username: '',
+    //         description: '',
+    //         duration: 0,
+    //         date: new Date(),
+    //         user: []
+    //     }
+    // }
+
+   
+    render() {
+        return(
+            <div>
+                <p>You are on the Create Exercises List component!</p>
+            </div>
+        )
+    };
+};
